838fcf2c86242c6dbdc8bffbd5d9f561e468155c
Yi Hyunjoon <yi.hyunjoon@gmail.com>
2020-02-05 01:13:28 +0900

    double quote to single quote (SC2139)

--- alias.sh
+++ alias.sh
@@ -1,19 +1,19 @@
-alias ...="../.."
-alias ....="../../.."
+alias ...='../..'
+alias ....='../../..'
 
-alias zp="zplugin"
-alias ls="exa"
-alias l="ls -al"
+alias zp='zplugin'
+alias ls='exa'
+alias l='ls -al'
 
-alias g="git"
-alias v="bat"
+alias g='git'
+alias v='bat'
 
-alias nv="nvim"
-alias em="emacs"
-alias th="trash"
+alias nv='nvim'
+alias em='emacs'
+alias th='trash'
 
 # Enable aliases to be sudo’ed
 alias sudo='sudo '
 
 # Reload the shell (i.e. invoke as a login shell)
-alias reload="exec ${SHELL} -l"
+alias reload='exec $(SHELL) -l'
