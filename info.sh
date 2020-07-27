84c4e64b73b3ed63119b61eb6fbcc92bdfc7f818
Yi Hyunjoon <yi.hyunjoon@gmail.com>
2020-07-27 23:00:28 +0900

    Remove scripts

--- info.sh
+++
@@ -1,9 +0,0 @@
-source ./color.sh;
-
-if [[ $(uname) = "Darwin" ]]; then
-    printf "${BOLD}OS${RESET}: macOS"
-elif [[ $(uname) = "Linux" ]]; then
-    echo "OS: Linux"
-else
-    echo "OS: Unknown"
-fi
