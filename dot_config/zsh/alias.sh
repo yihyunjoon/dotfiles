a14d660c1598cb070e2fd1e6249f09b303a8a35c
Yi Hyunjoon <yi.hyunjoon@gmail.com>
2025-12-12 00:23:32 +0900

    chezmoi

---
+++ dot_config/zsh/alias.sh
@@ -0,0 +1,28 @@
+alias ...='../..'
+alias ....='../../..'
+
+alias l='eza -al'
+
+# Git and GitHub
+alias g='git'
+alias gs='git switch'
+alias gp='git push'
+alias gc='gh repo clone'
+
+alias v='bat'
+
+alias r='cargo'
+alias u='uv run'
+
+alias c='code'
+alias nv='nvim'
+alias th='trash'
+
+# Enable aliases to be sudo’ed
+alias sudo='sudo '
+
+# Reload the shell (i.e. invoke as a login shell)
+alias reload='exec $(SHELL) -l'
+
+alias ij='zellij'
+alias zm='chezmoi'
---
+++ dot_config/zsh/dot_zprofile
@@ -0,0 +1,11 @@
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init.zsh 2>/dev/null || :
+
+# Setting PATH for Python 3.13
+# The original version is saved in .zprofile.pysave
+PATH="/Library/Frameworks/Python.framework/Versions/3.13/bin:${PATH}"
+export PATH
